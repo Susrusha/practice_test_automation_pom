d0c65368d8816908ce333cbefc931e43063061ae
Susrusha <nagetisusrusha@gmail.com>
2026-04-19 09:59:20 +0530

    added screenshot capture for failed tests

--- config/config.py
+++ config/config.py
@@ -1,4 +1,3 @@
 base_url="https://practicetestautomation.com/practice-test-login/"
 
 redirected_url="https://practicetestautomation.com/logged-in-successfully/"
-
--- conftest.py
+++ conftest.py
@@ -1,11 +1,12 @@
 import pytest
+import allure
 from config.config import base_url
 from utils.logger import get_logger
 
 from selenium import webdriver
 
 # @pytest.fixture() # open browser, run one test, close browser. repeat
-@pytest.fixture(scope="session") # open browser once, run all tests, close browser
+@pytest.fixture(scope="function")
 def setup():
     driver=webdriver.Chrome()
     logger = get_logger()
@@ -17,5 +18,22 @@ def setup():
     yield driver
     logger.info("Browser Closed")
     driver.quit()
+
+# ----------- HOOK -----------------
+@pytest.hookimpl(hookwrapper=True) # let test finish
+def pytest_runtest_makereport(item, call): # run every test
+    outcome = yield # pause
+    result = outcome.get_result()
+    # only when test fails in execution
+
+    if result.when == "call" and result.failed:
+        driver = item.funcargs.get("setup")
+        if driver:
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name = "Failed Screenshots",
+                attachment_type = allure.attachment_type.PNG
+            )
+
 
     
